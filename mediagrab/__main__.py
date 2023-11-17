13cb1fe078795c1e3f8ba2ef2e9a75162cc5d982
mrprikol <95529270+mrprikol@users.noreply.github.com>
2023-11-17 23:54:43 +0200

    Move all functionality to main module

---
+++ mediagrab/__main__.py
@@ -0,0 +1,15 @@
+import sys
+
+import pytube
+
+
+def main():
+    video_url = sys.argv[1]
+
+    yt = pytube.YouTube(video_url)
+    stream = yt.streams.get_highest_resolution()
+
+    stream.download()
+
+
+main()
